88f8923da4b54b50b07eecca84178f9c86e76615
kkh <rudgus327@ecoletree.com>
2023-08-30 14:49:28 +0900

    edit webview

--- lib/body/webviewNaverPage.dart
+++ lib/body/webviewNaverPage.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-// import 'package:webview_flutter_android/webview_flutter_android.dart';
-// import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
 class WebviewNaverPage extends StatelessWidget {
   const WebviewNaverPage({super.key});
@@ -9,7 +7,7 @@ class WebviewNaverPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body:  WebView( ///3.0.4 이후에 WebView() 클래스
+      body:  WebView(
         initialUrl: 'https://www.naver.com/',
         javascriptMode: JavascriptMode.unrestricted,
         gestureNavigationEnabled: true,
